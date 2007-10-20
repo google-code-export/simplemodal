00f7d1aaf62e9c2cb4d4d0eeb183601205c78a41
emartin24 <emartin24@a759731f-a13b-0410-a66d-31dce0b9f610>
2007-10-20 14:51:56 +0000

    Updating demos - still a work in progress

---
+++ trunk/demo/data/contact.php
@@ -0,0 +1,29 @@
+<?php
+
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+if (empty($action)) {
+    echo "<div id='contactForm' style='display:none'>
+  <h1>Contact Us</h1>
+  <form action='#'>
+    <div class='loading' style='display:none'></div>
+    <div class='message' style='display:none'></div>
+    <label for='name'>Name:</label>
+    <input type='text' id='name' name='name' size='30'/>
+    <label for='email'>Email:</label>
+    <input type='text' id='email' name='email' size='40'/>
+    <label for='message'>Message:</label>
+    <textarea id='message' name='message' cols='30' rows='5'></textarea>
+    <br/>
+    <label for='submit'>&nbsp;</label>
+    <input type='submit' id='submit' name='submit' value='Send'/>
+    <input type='button' class='modalClose' value='Cancel'/>
+  </form>
+</div>";
+}
+else if ($action == 'send') {
+    print 'Message successfully sent.';
+}
+exit;
+
+?>
