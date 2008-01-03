82a4427c8fae5632d4939af2ee8ab5399b5a2fab
emartin24 <emartin24@a759731f-a13b-0410-a66d-31dce0b9f610>
2008-01-03 21:20:09 +0000

    Demo and SMCF changes

--- demo/js/contact.js
+++ demo/js/contact.js
@@ -40,9 +40,19 @@ var contact = {
 				});
 				// resize the textarea for safari
 				if ($.browser.safari) {
+					$('#contactModalContainer #name, #contactModalContainer #email').css({
+						'font-size': '.9em'
+					}).attr({size: '36'});
 					$('#contactModalContainer textarea').attr({
-						cols: '37',
-						rows: '8'
+						cols: '34',
+						rows: '7'
+					});
+				}
+				// resize the textarea for ie
+				if ($.browser.msie) {
+					$('#contactModalContainer textarea').css({
+						width: '254px',
+						height: '96px'
 					});
 				}
 			});
@@ -117,9 +127,7 @@ var contact = {
 			contact.message += 'Email is required. ';
 		}
 		else {
-			// Regex from: http://regexlib.com/REDetails.aspx?regexp_id=599
-			var filter = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/;
-			if (!filter.test(email)) {
+			if (!contact.validateEmail(email)) {
 				contact.message += 'Email is invalid. ';
 			}
 		}
@@ -135,7 +143,48 @@ var contact = {
 			return true;
 		}
 	},
+	validateEmail: function (email) {
+		var at = email.lastIndexOf("@");
+
+		// Make sure the at (@) sybmol exists and  
+		// it is not the first or last character
+		if (at < 1 || (at + 1) === email.length)
+			return false;
+
+		// Make sure there aren't multiple periods together
+		if (/(\.{2,})/.test(email))
+			return false;
+
+		// Break up the local and domain portions
+		var local = email.substring(0, at);
+		var domain = email.substring(at + 1);
+
+		// Check lengths
+		if (local.length < 1 || local.length > 64 || domain.length < 4 || domain.length > 255)
+			return false;
+
+		// Make sure local and domain don't start with or end with a period
+		if (/(^\.|\.$)/.test(local) || /(^\.|\.$)/.test(domain))
+			return false;
+
+		// Check for quoted-string addresses
+		// Since almost anything is allowed in a quoted-string address,
+		// we're just going to let them go through
+		if (!/^"(.+)"$/.test(local)) {
+			// It's a dot-string address...check for valid characters
+			if (!/^[-a-zA-Z0-9!#$%*\/?|^{}`~&'+=_\.]*$/.test(local))
+				return false;
+		}
+
+		// Make sure domain contains only valid characters and at least one period
+		if (!/^[-a-zA-Z0-9\.]*$/.test(domain) || domain.indexOf(".") === -1)
+			return false;	
+
+		return true;
+	},
 	showError: function () {
-		$('#contactModalContainer .message').html($('<div class="error"></div>').append(contact.message)).fadeIn(200);
+		$('#contactModalContainer .message')
+			.html($('<div class="error">').append(contact.message))
+			.fadeIn(200);
 	}
 };
