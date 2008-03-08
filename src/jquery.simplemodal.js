3d18a248e9cdced5a26cf7f3fc2a60500cb993c9
emartin24 <emartin24@a759731f-a13b-0410-a66d-31dce0b9f610>
2008-03-08 00:04:10 +0000

    - More updates to SimpleModal - Fixed issue with google analytics code - Started updating tests and demos

--- src/jquery.simplemodal.js
+++ src/jquery.simplemodal.js
@@ -1,13 +1,71 @@
+/*
+ * SimpleModal @VERSION - jQuery Plugin
+ * http://www.ericmmartin.com/projects/simplemodal/
+ * http://plugins.jquery.com/project/SimpleModal
+ * http://code.google.com/p/simplemodal/
+ *
+ * Copyright (c) 2008 Eric Martin - http://ericmmartin.com
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * Revision: $Id$
+ *
+ *
+ * TODO:
+ * - handling content from dom - clone content/parent/etc
+ * - add close image or link?
+ * - esc key w/ multiple dialogs? option: multiple?
+ * - onClose event && recursion
+ * - documentation
+ * - search TODO's and resolve
+ * - consider autoopen option?
+ */
+ 
+/**
+ * SimpleModal is a lightweight jQuery plugin that provides a simple
+ * interface to create a modal dialog.
+ *
+ * TODO - finish writeup
+ *
+ * SimpleModal has been tested in the following browsers:
+ * - IE 6, 7
+ * - Firefox 2, 3
+ * - Opera 9
+ * - Safari 3
+ *
+ * @name SimpleModal
+ * @type jQuery
+ * @requires jQuery v1.1.2 (TODO - check this)
+ * @cat Plugins/Windows and Overlays
+ * @author Eric Martin (http://ericmmartin.com)
+ * @version @VERSION
+ */
 (function () {
+
+	/**
+	 * "Chained" function that takes an optional options object. It instantiates a
+	 * modal dialog for every matching element.
+	 *
+	 * @param {object} [options] An optional object containing options overrides
+	 */
 	$.fn.modal = function (options) {
 		return this.each(function () {
 			new $.modal.dialog($(this), options);
 		});
 	};
 
+	/*
+	 * "Stand-alone" function that takes a "data" object and an optional options object. 
+	 * If obj is not a jQuery object, this function will convert it to a one and then 
+	 * invoke the "chained" modal function.
+	 * 
+	 * @param {Object} obj A string, jQuery object or DOM object
+	 * @param {Object} [options] An optional object containing options overrides
+	 */
 	$.modal = function (obj, options) {
 		var element;
-		
+
 		// determine the datatype for content and handle accordingly
 		if (typeof obj == 'object') {
 			// convert to a jQuery object, if necessary
@@ -19,24 +77,35 @@
 		}
 		else {
 			// unsupported data type
-			if (window.console) {
-				console.log('SimpleModal Error: Unsupported data type: ' + typeof obj);
-			}
+			window.console &&console.log('SimpleModal Error: Unsupported data type: ' + typeof obj);
 			return false;
 		}
 		return element.modal(options);
 	};
 
+	/*
+	 * SimpleModal default options
+	 *
+	 * zIndex: (Number:2000) The default z-index value
+	 * overlayId: (String:'') The DOM element id for the overlay div
+	 * overlayCss: (Object:{}) The CSS styling for the overlay div
+	 * containerId: (String:'') The DOM element id for the container div
+	 * containerCss: (Object:{}) The CSS styling for the container div
+	 * contentId: (String:'') The DOM element id for the content div
+	 * contentCss: (String:{}) The CSS styling for the content div
+	 * onOpen: (Function:null) The callback function used in place of SimpleModal's open
+	 * onShow: (Function:null) The callback function used after the modal dialog has opened
+	 * onClose: (Function:null) The callback function used in place of SimpleModal's close
+	 * bgiframe: (Boolean:false) Use the bgiframe plugin, if available, to fix IE6 issues
+	 */
 	$.modal.defaults = {
-		onOpen: null,
-		onShow: null,
-		onClose: null,
-		bgiframe: false,
 		zIndex: 2000,
+		overlayId: '',
 		overlayCss: {
 			opacity: .5,
 			background: '#000'
 		},
+		containerId: '',
 		containerCss: {
 			background: '#fff',
 			border: '2px solid #ccc',
@@ -46,127 +115,147 @@
 			height: '400px',
 			width: '400px'
 		},
+		contentId: '',
 		contentCss: {
 			overflow: 'auto',
 			width: '100%',
 			height: '100%'
-		}
+		},
+		onOpen: null,
+		onShow: null,
+		onClose: null,
+		bgiframe: false
 	};
-	
+
 	$.modal.dialog = function (element, options) {
 		this.options = $.extend({}, $.modal.defaults, options);
 		
 		this.ie6 = $.browser.msie && $.browser.version < 7 ? true : false;
-		this.id = $.data(this, 'simplemodal-dialog');
+		this.id = $.data(this);
+
+		// save some data about the state of the body
 		this.body = {};
 		this.body.height = $('body').css('height') || $('body').height();
 		this.body.width = $('body').css('width') || $('body').width();
 		this.body.overflow = $('body').css('overflow') || 'visible';
-		this.body.scrollOffset = window.pageYOffset || $.boxModel && document.documentElement.scrollTop || document.body.scrollTop;
-			
+
+		// save some data about the state of the window
+		this.window = {};
+		this.window.height = $(window).height();
+		this.window.width = $(window).width();
+		this.window.scrollOffset = window.pageYOffset || $.boxModel && document.documentElement.scrollTop || document.body.scrollTop;
+
 		this.overlay = $('<div class="simplemodal-overlay"/>')
+			.attr('id', this.options.overlayId)
 			.css($.extend(this.options.overlayCss, {
-				height: '100%',
-				width: '100%',
-				position: 'fixed',
+				height: this.window.height,
+				width: this.window.width,
+				position: 'absolute',
 				left: 0,
 				top: 0,
 				zIndex: ++this.options.zIndex
 			}))
-			.hide()
-			.appendTo('body');
-				
+			.hide();
+
 		this.container = $('<div class="simplemodal-container"/>')
+			.attr('id', this.options.containerId)
 			.css($.extend(this.options.containerCss, {
-				position: 'fixed',
+				position: 'absolute',
 				zIndex: ++this.options.zIndex
 			}))
-			.hide()
-			.appendTo('body');
-			
+			.hide();
+
 		this.content = $('<div class="simplemodal-content"/>')
+			.attr('id', this.options.contentId)
 			.css($.extend(this.options.contentCss, {}))
-			.append(element)
+			.append(element.show())
 			.appendTo(this.container)
 			.hide();
-			
+
 		// fix IE 6 issues
 		if (this.ie6) {
 			// add an iframe to fix z-index issues
 			this.options.bgiframe && $.fn.bgiframe 
 				? this.overlay.bgiframe() // use bgiframe, if available
-				: this.iframe = $('<iframe src="javascript:false;" class="simplemodal-iframe">')
-					.css($.extend({}, {
-						opacity: 0, 
-						position: 'absolute',
-						height: '100%',
-						width: '100%',
-						zIndex: this.overlay.css('zIndex') - 1,
-						top: 0,
-						left: 0
-					}))
-					.hide()
-					.appendTo('body');
+				: _fixIe6.apply(this);
 		}
 
 		this.open();
-		
+
 		// increment z-index to ensure uniqueness
 		$.modal.defaults.zIndex += 5;
 	};
 
 	$.modal.dialog.prototype = {
-		open: function () {			
+		open: function () {
 			$('body').css({
-				height: $(window).height(),
-				width: $(window).width()
+				height: this.window.height,
+				width: this.window.width
 			});
 
-			$('body').contents().wrapAll(
-				$('<div class="simplemodal-body-wrap"/>')
-					.css({
-						overflow: 'hidden',
-						height: $(window).height(),
-						width: $(window).width()
-					})
-			);
-			$('.simplemodal-body-wrap')[0].scrollTop = this.body.scrollOffset;
+			if ($('.simplemodal-wrap').length == 0) {
+				$('body').contents().wrapAll(
+					$('<div class="simplemodal-wrap"/>')
+						.css({
+							overflow: 'hidden',
+							height: this.window.height,
+							width: this.window.width
+						})
+				);
+				_addDialog.apply(this);
+				$('.simplemodal-wrap')[0].scrollTop = this.window.scrollOffset;
+			}
+			else {
+				_addDialog.apply(this);
+			}
 
 			$.isFunction(this.options.onOpen) 
 				? this.options.onOpen.apply(this, [this]) 
-				: this.content.show() 
-					&& this.container.show() 
-					&& this.overlay.show()
-					&& (this.ie6 && this.iframe.show());
+				: _showDialog.apply(this);
 
 			// onShow callback
 			$.isFunction(this.options.onShow) && this.options.onShow.apply(this, [this]);
-			
-			// bind close event(s)
-			var self = this;
-			this.overlay.bind('click.simplemodal-' + this.id, function (e) {
-				e.preventDefault();
-				self.close();
-			});
-			$(window).bind('keypress.simplemodal-' + this.id, function (e) {
-				e.preventDefault();
-				e.keyCode == 27 && self.close();
-			});
+
+			_bindEvents.apply(this);
 		},
 		close: function () {
-			$.isFunction(this.options.onClose) 
-				? this.options.onClose.apply(this, [this]) 
-				: this.content.hide() 
-					&& this.container.hide() 
-					&& this.overlay.hide()
-					&& (this.ie6 && this.iframe.hide());
-					
-			this.content.remove() 
-					&& this.container.remove() 
-					&& this.overlay.remove()
-					&& (this.ie6 && this.iframe.remove());
-					
-			var wrap = $('.simplemodal-body-wrap');
+			_unbindEvents.apply(this);
+			
+			if ($.isFunction(this.options.onClose)) {
+				this.options.onClose.apply(this, [this]);
+			}
+			else {
+				_hideDialog.apply(this);
+				this.remove();
+			}
+		},
+		remove: function () {
+			_removeDialog.apply(this);
+		}
+	};
+
+	// private functions
+	function _addDialog () {
+		this.ie6 && this.iframe.appendTo('body');
+		this.overlay.appendTo('body');
+		this.container.appendTo('body');
+	}
+
+	function _hideDialog () {
+		this.content.hide();
+		this.container.hide(); 
+		this.overlay.hide();
+		this.ie6 && this.iframe.hide();
+	}
+
+	function _removeDialog () {
+		this.content.remove();
+		this.container.remove();
+		this.overlay.remove();
+		this.ie6 && this.iframe.remove();
+
+		if ($('.simplemodal-overlay').length == 0) {
+			var wrap = $('.simplemodal-wrap');
 			wrap.contents().appendTo(
 				$('body').css({
 					height: this.body.height,
@@ -174,10 +263,99 @@
 				})
 			);
 			wrap.remove();
-			window.scroll(0, this.body.scrollOffset);
-			
-			this.overlay.unbind('click.simplemodal-' + this.id);
-			$(window).unbind('keypress.simplemodal-' + this.id);
+			window.scroll(0, this.window.scrollOffset);
+		}
+	} 
+
+	/**
+	 * Handles the showing of each of the modal dialog elements using the jQuery show()
+	 * function. The iframe will only be shown if the browser is IE6.
+	 * This function also sets focus to the content div immediately after the dialog
+	 * is opened.
+	 * TODO - move the iframe show and focus out, otherwise a callback will miss it
+	 */
+	function _showDialog () {
+		this.ie6 && this.iframe.show();
+		this.overlay.show();
+		this.container.show();
+		this.content.show();
+		this.content.focus();
+	}
+
+	/**
+	 * Handles the binding of events for the current modal dialog. 
+	 * Any element that includes the simplemodal-close class will have the click 
+	 * event bound to it, which will fire the close event for the dialog. The ESC
+	 * key will also be bound to the close event.
+	 * The TAB and ENTER key's will be watched to prevent circumvention of the
+	 * modal dialog. See the _keyWatch function.
+	 */
+	function _bindEvents () {
+		var dialog = this;
+		
+		// click overlay to close (TODO - make this an option or remove)
+		this.overlay.bind('click.simplemodal-overlay-' + this.id, function (e) {
+			e.preventDefault();
+			dialog.close();
+		});
+
+		// click to close any element with a class of simplemodal-close
+		this.container.find('.simplemodal-close').bind('click.simplemodal-close-' + this.id, function (e) {
+			e.preventDefault();
+			dialog.close();
+		});
+
+		// watch TAB - used keyup because IE doesn't recognize keypress and keydown fires too early
+		$().bind('keyup.simplemodal-keyup', function (e) {
+			e.keyCode == 9 && _keyWatch.apply(dialog.content, [e]); // tab
+		});
+
+		// watch ESC and ENTER - again, can't use keypress (IE) and keyup fires to late (ENTER)
+		$().bind('keydown.simplemodal-keydown', function (e) {
+			e.keyCode == 13 && _keyWatch.apply(dialog.content, [e]); // enter
+			e.keyCode == 27 && dialog.close(); // esc
+		});
+	}
+
+	/**
+	 * Unbinds the events that were added in the _bindEvents function.
+	 */
+	function _unbindEvents () {
+		this.overlay.unbind('click.simplemodal-overlay' + this.id);
+		this.container.find('.simplemodal-close').unbind('click.simplemodal-close' + this.id);
+		$().unbind('keyup.simplemodal-keyup').unbind('keydown.simplemodal-keydown');
+	}
+
+	/**
+	 * Checks to see if the target of the passed in key event is a child of a
+	 * SimpleModal content element. If not, it cancels the event and returns 
+	 * focus to the SimpleModal content element.
+	 * This prevents a user from triggering an event that is supposed to be
+	 * hidden by the modal dialog.
+	 * @param {Object} e A key* event 
+	 */
+	function _keyWatch (e) {
+		if (!$(e.target).parents().andSelf().hasClass('simplemodal-content')) {
+			e.preventDefault();
+			this.focus();
 		}
 	}
+
+	/**
+	 *
+	 */
+	function _fixIe6 () {
+		this.iframe = $('<iframe src="javascript:false;" class="simplemodal-iframe">')
+			.css($.extend({}, {
+				opacity: 0, 
+				position: 'absolute',
+				height: this.window.height,
+				width: this.window.width,
+				zIndex: this.overlay.css('zIndex') - 1,
+				top: 0,
+				left: 0
+			}))
+			.hide();
+	}
+
 })(jQuery);
--- test/js/simplemodal.js
+++ test/js/simplemodal.js
@@ -72,8 +72,7 @@ $(document).ready(function () {
 	$('a#test13a').click(function (e) {
 		e.preventDefault();
 		$('#modalContentTest').modal({
-			overlayCss: {opacity: 1, backgroundColor: '#fff'},
-			containerCss: {top: '5%', left: '5%', marginLeft: 0}
+			overlayCss: {opacity: 1, backgroundColor: '#fff'}
 		});
 	});
    $('a#test14').click(function (e) {
@@ -84,34 +83,34 @@ $(document).ready(function () {
 
 /**
  * When the open event is called, this function will be used to 'open'
- * the overlay, container and data portions of the modal dialog.
+ * the overlay, container and content portions of the modal dialog.
  *
  * onOpen callbacks need to handle 'opening' the overlay, container
- * and data.
+ * and content.
  */
 function modalOpen (dialog) {
 	dialog.overlay.fadeIn('slow', function () {
 		dialog.container.fadeIn('slow', function () {
-			dialog.data.slideDown('slow');
+			dialog.content.slideDown('slow');
 		});
 	});
 }
 
 /**
  * When the close event is called, this function will be used to 'close'
- * the overlay, container and data portions of the modal dialog.
+ * the overlay, container and content portions of the modal dialog.
  *
  * The SimpleModal close function will still perform some actions that
  * don't need to be handled here.
  *
  * onClose callbacks need to handle 'closing' the overlay, container
- * data and iframe.
+ * content and iframe.
  */
 function modalClose (dialog) {
-	dialog.data.fadeOut('slow', function () {
+	dialog.content.fadeOut('slow', function () {
 		dialog.container.hide('slow', function () {
 			dialog.overlay.slideUp('slow', function () {
-				dialog.close();
+				dialog.remove();
 			});
 		});
 	});
@@ -119,15 +118,15 @@ function modalClose (dialog) {
 
 /**
  * After the dialog is show, this callback will bind some effects
- * to the data when the 'button' button is clicked.
+ * to the content when the 'button' button is clicked.
  *
  * This callback is completely user based; SimpleModal does not have
  * a matching function.
  */
 function modalShow (dialog) {
-	dialog.data.find('input.animate').one('click', function () {
-		dialog.data.slideUp('slow', function () {
-			dialog.data.slideDown('slow');
+	dialog.content.find('input.animate').one('click', function () {
+		dialog.content.slideUp('slow', function () {
+			dialog.content.slideDown('slow');
 		});
 	});
 }
