f4ad92272924404b9aff148d435cc51ffb5bab37
emartin24 <emartin24@a759731f-a13b-0410-a66d-31dce0b9f610>
2008-12-12 22:43:48 +0000

    tagging v1.2.1

---
+++ tags/1.2.1/demo/js/basic.js
@@ -0,0 +1,20 @@
+/*
+ * SimpleModal Basic Modal Dialog
+ * http://www.ericmmartin.com/projects/simplemodal/
+ * http://code.google.com/p/simplemodal/
+ *
+ * Copyright (c) 2008 Eric Martin - http://ericmmartin.com
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Revision: $Id$
+ *
+ */
+
+$(document).ready(function () {
+	$('#basicModal input.basic, #basicModal a.basic').click(function (e) {
+		e.preventDefault();
+		$('#basicModalContent').modal();
+	});
+});
